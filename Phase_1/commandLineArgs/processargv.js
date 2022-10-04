936031333fde633c43e4636a3f46653bf368f049
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-04 18:58:03 -0400

    Add 2 numbers from command line and print result to console

--- Phase_1/commandLineArgs/processargv.js
+++ Phase_1/commandLineArgs/processargv.js
@@ -1,2 +1,11 @@
-const args = process.argv;
-console.log(args);
+// add 2 numbers inputted from command line and print the total sum
+
+const inputs = process.argv.slice(2);
+
+let result = 0;
+
+for (const input of inputs) {
+  
+  result += +input; //addubg a + in front of a variable name will change it to a number.
+}
+console.log(result); 
