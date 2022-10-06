c53be5f5298a7aa696c5f90404761905fa27820e
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-05 23:29:38 -0400

    Pass assert equal test to make sure the original array is not changed and the tail is returned

--- Phase_1/loblow/tail.js
+++ Phase_1/loblow/tail.js
@@ -5,16 +5,17 @@ const assertEqual = require('./assertEqual');
 
 const tail = (array) => {
   const result = array.slice(1);
-  const originalArrayContents = ""
+  let originalArrayContents = ""
   for (let item of array) {
     originalArrayContents += item.toString();
   };
-  const tailContents = "";
+  let tailContents = "";
   for (let item of result) {
     tailContents += item.toString();
   }
 
-  return `Orignal array is ${originalArrayContents}; Tail of the array is ${tailContents}`
+  return `Original array is ${originalArrayContents}; Tail of the array is ${tailContents}`
 }
 
 //TEST CODE
+assertEqual(tail([3,5,7,8]), `Original array is 3578; Tail of the array is 578`);
