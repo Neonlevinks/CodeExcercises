fb87ae8a9a64cf39d7f66b14e824272d4b53222d
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-05 23:00:46 -0400

    Implement head function with test code

--- Phase_1/loblow/head.js
+++ Phase_1/loblow/head.js
@@ -2,3 +2,11 @@
 
 import assertEqual from "./assertEqual"
 
+const head = (array) => {
+  result = array[0];
+  return result;
+}
+
+//TEST CODE
+assertEqual(head([5,6,7]), 5); 
+assertEqual(head(["Hello", "Mister", "Nelson"]), "Hello");
