8e03448b8a95eb0324020eafffa3e5e31ce32635
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-05 23:30:43 -0400

    Remove assertion test, export tail function

--- Phase_1/loblow/head.js
+++ Phase_1/loblow/head.js
@@ -1,7 +1,5 @@
 // write a function that returns the head, (the first item), of an array.
 
-const assertEqual = require('./assertEqual')
-
 const head = (array) => {
   result = array[0];
   return result;
--- Phase_1/loblow/tail.js
+++ Phase_1/loblow/tail.js
@@ -1,21 +1,8 @@
 // Make a function that returns the tail of an array
 
-const assertEqual = require('./assertEqual');
-
-
 const tail = (array) => {
   const result = array.slice(1);
-  let originalArrayContents = ""
-  for (let item of array) {
-    originalArrayContents += item.toString();
-  };
-  let tailContents = "";
-  for (let item of result) {
-    tailContents += item.toString();
-  }
-
-  return `Original array is ${originalArrayContents}; Tail of the array is ${tailContents}`
+  return result;
 }
 
-//TEST CODE
-assertEqual(tail([3,5,7,8]), `Original array is 3578; Tail of the array is 578`);
+module.exports = tail
