4d15faa0a47d53c8e1f038515969d08084e702eb
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-05 23:17:56 -0400

    Implement tail function. Write a simple test

--- Phase_1/loblow/tail.js
+++ Phase_1/loblow/tail.js
@@ -1,3 +1,12 @@
 // Make a function that returns the tail of an array
 
 const assertEqual = require('./assertEqual');
+
+const tail = (array) => {
+  const result = array.slice(1);
+  console.log(array);
+  console.log(result);
+}
+
+//TEST CODE
+tail([3,5,7,8]);
