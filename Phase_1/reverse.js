01c3a07e819e0b95f8381290b4387a17d73098d6
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-06 17:32:16 -0400

    Implement a function to take command line args, and use the reverse function on each arg

--- Phase_1/reverse.js
+++ Phase_1/reverse.js
@@ -1,13 +1,21 @@
 // Write a function that returns strings from the command line in reverse
 
+//This function strictly reverses any string and prints it
 const reverse = (string) => {
-  console.log(string);
   let result = '';
   for (let i = string.length -1 ; i >= 0; i--) {
     result += string[i];
   }
   console.log(result);
-}
+  return result;
+;}
 
-//TEST CODE
-reverse("hello");
+//This funtion will take in the command line args, and apply the reverse function to each arg in the array
+const reverseArgs = (array) => {
+  for (let item of array) {
+    reverse(item);
+  }
+};
+
+const args = process.argv.slice(2);
+reverseArgs(args);
