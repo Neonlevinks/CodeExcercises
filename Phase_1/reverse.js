95dfccb13c3987bb2f4806cf66f1acde11640a61
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-06 17:24:01 -0400

    Print the reverse of any string

--- Phase_1/reverse.js
+++ Phase_1/reverse.js
@@ -1,8 +1,13 @@
 // Write a function that returns strings from the command line in reverse
 
-const reverse(inputArray) = {
-
+const reverse = (string) => {
+  console.log(string);
+  let result = '';
+  for (let i = string.length -1 ; i >= 0; i--) {
+    result += string[i];
+  }
+  console.log(result);
 }
 
 //TEST CODE
-reverse(['a', 'b', 'c']);
+reverse("hello");
