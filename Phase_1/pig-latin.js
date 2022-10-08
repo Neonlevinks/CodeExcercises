ccf790c6760336bcafd42dc6c468a2ad850fd397
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-07 22:36:19 -0400

    Task: Write a function that takes string args and changes them to pig latin

---
+++ Phase_1/pig-latin.js
@@ -0,0 +1,12 @@
+// Write a function that returns strings from the command line in pig latin
+// pig latin takes a word and outputs a word that starts with the tail, then the head, then has ay added. 
+// for example, pig would become igpay.
+
+const pigLatin = () => {
+  const result = '';
+  return result;
+}
+
+
+// TEST CODE
+pigLatin('pig') // should return 'igpay'
