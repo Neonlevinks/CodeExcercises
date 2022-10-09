4e41b123c2cfb11a2e2d8b57ec937ff439422e51
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-08 23:36:03 -0400

    Use command line args instead of hard coded parameters for roll function

--- Phase_1/dice-roller.js
+++ Phase_1/dice-roller.js
@@ -5,11 +5,11 @@ const roll = (totalDice) => {
 
   for (let currentDice = 1; currentDice <= totalDice ; currentDice++) {
 
-    currentDice === totalDice ? allRolls += `${Math.floor(Math.random()*6 + 1)}`: allRolls += `${Math.floor(Math.random()*6 + 1)}, `;
+    currentDice == totalDice ? allRolls += `${Math.floor(Math.random()*6 + 1)}`: allRolls += `${Math.floor(Math.random()*6 + 1)}, `;
 
   }
   return console.log(`Rolled ${totalDice} dice: ${allRolls}`);
 };
 
-//TEST CODE
-roll(2);
+const args = process.argv.slice(2);
+roll(args);
