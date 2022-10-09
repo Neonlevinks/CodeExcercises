f08f567478378fd1a0bc48b660ddf5040939dcd2
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-08 23:16:50 -0400

    Task: Write a program that takes a number and rulls that many 6 sided dice

---
+++ Phase_1/dice-roller.js
@@ -0,0 +1,9 @@
+//Write a program that takes a single number from the command line, and rolls that many six sided dice
+
+const roll = (totalDice) => {
+  const allRolls = "";
+  return allRolls;
+};
+
+//TEST CODE
+roll(2);
