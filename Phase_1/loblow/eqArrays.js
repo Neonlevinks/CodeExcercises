61742d8360718be659507651327994d5364a4ea9
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-10 08:21:54 -0400

    Write Write a function that takes 2 arrays and print an appropriate message to the console

--- Phase_1/loblow/eqArrays.js
+++ Phase_1/loblow/eqArrays.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 //Write a function that checks if 2 arrays are equal
 const eqArrays = (array1, array2) => {
   let array1Contents = "";
@@ -13,6 +11,4 @@ const eqArrays = (array1, array2) => {
   return array1Contents === array2Contents ? true : false;
 }
 
-//TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["a", 2, 3], ["a", 2, 3]), true);
+module.exports = eqArrays;
