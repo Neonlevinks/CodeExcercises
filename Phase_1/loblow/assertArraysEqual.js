7de33d6b672a6044fa1e63082dcaa1e87d887131
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-10 08:25:42 -0400

    Implement fuction with 2 test cases, one pass and one fail

--- Phase_1/loblow/assertArraysEqual.js
+++ Phase_1/loblow/assertArraysEqual.js
@@ -1,1 +1,12 @@
 const eqArrays = require('./eqArrays');
+
+//Write a function that takes 2 arrays and print an appropriate message to the console: 
+
+const assertArraysequal = (array1, array2) => {
+  const isArrayEqual = eqArrays(array1, array2);
+  isArrayEqual ? console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`) : console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`)
+};
+
+// TEST CODE
+assertArraysequal((17,4,9), (17,4,9)) //Should pass
+assertArraysequal((17,4,9), (1,23,41)) // Should fail
