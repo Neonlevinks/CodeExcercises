ef025c6121ac543019cdeb22df05d63c5d17b5dd
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-09 22:57:38 -0400

    Loop through each array, store all contents in a string, then compare strings and return true/false based on comparison

--- Phase_1/loblow/eqArrays.js
+++ Phase_1/loblow/eqArrays.js
@@ -1,2 +1,17 @@
 const assertEqual = require('./assertEqual');
 
+//Write a function that checks if 2 arrays are equal
+const eqArrays = (array1, array2) => {
+  let array1Contents = "";
+  let array2Contents = "";
+  for (let item of array1) {
+    array1 += item.toString();
+  };
+  for (let item of array2) {
+    array2 += item.toString();
+  }
+  return array1Contents === array2Contents ? true : false;
+}
+
+//TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
