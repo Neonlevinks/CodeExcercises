8c03046097797f5febeb627892e2998dcb5299d0
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-17 21:14:55 -0400

    Write empty function, assign an array some strings, set up a test case to check if the array matches the compontes in another array

---
+++ Phase_1/loblow/without.js
@@ -0,0 +1,13 @@
+//Write a function that removes specific items from an array
+
+const assertArraysequal = require("./assertArraysEqual");
+
+const words = ["hello", "world", "lighthouse"]
+const without = (startingArray, removalItems) => {
+  const resultArray = [];
+
+};
+
+without(words, "lighthouse");
+
+assertArraysequal(words, ["hello", "world", "lighthouse"]);
