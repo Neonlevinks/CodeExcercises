8d3f09e3466e55730eaed215311c06a66dd49893
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-17 21:17:58 -0400

    Remove test code from assertArraysEqual

--- Phase_1/loblow/assertArraysEqual.js
+++ Phase_1/loblow/assertArraysEqual.js
@@ -7,6 +7,3 @@ const assertArraysequal = (array1, array2) => {
   isArrayEqual === true ? console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`) : console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`)
 };
 
-// TEST CODE
-assertArraysequal(([17,4,9]), ([17,4,9])) //Should pass
-assertArraysequal(([17,4,9]), ([1,23,41])) // Should fail
