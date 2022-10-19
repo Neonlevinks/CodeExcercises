265b4b4d75d8449810ec599a361fff5a0d65b777
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-18 20:49:22 -0400

    Fix test code

--- Phase_1/loblow/eqArrays.js
+++ Phase_1/loblow/eqArrays.js
@@ -2,12 +2,17 @@
 const eqArrays = (array1, array2) => {
   let array1Contents = "";
   let array2Contents = "";
-  for (let item of array1) {
-    array1Contents += item.toString();
-  };
-  for (let item of array2) {
-    array2Contents += item.toString();
+  if (array1) {
+    for (let item of array1) {
+      array1Contents += item.toString();
+    };
   }
+  if (array2) {
+    for (let item of array2) {
+      array2Contents += item.toString();
+    }
+  }
+  
   return array1Contents === array2Contents ? true : false
 }
 
--- Phase_1/loblow/middle.js
+++ Phase_1/loblow/middle.js
@@ -11,5 +11,5 @@ const middle = (array) => {
 //TEST CODE
 assertArraysequal(middle([1]), []); //pass, no middle of 1 item array
 assertArraysequal(middle([1, 2]), []); //pass, no middle of 2 item array
-assertArraysequal(middle([1, 2, 3], [2])); //pass, middle is 2
-assertArraysequal(middle([1, 2, 3, 4], [2, 3])) //pass, middle is 2, 3
+assertArraysequal(middle([1, 2, 3]), [2]); //pass, middle is 2
+assertArraysequal(middle([1, 2, 3, 4]), [2, 3]) //pass, middle is 2, 3
