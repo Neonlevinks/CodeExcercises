ab774759e170c157a62e83de127f6be6fe7777d7
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-18 21:00:26 -0400

    Remove test code, export middle function

--- Phase_1/loblow/middle.js
+++ Phase_1/loblow/middle.js
@@ -1,7 +1,5 @@
 // Write a function that returns the middle of an array
  
-const assertArraysequal = require('./assertArraysEqual');
-
 const middle = (array) => {
   const result = [];
 
@@ -21,8 +19,4 @@ const middle = (array) => {
   return result;
 }
 
-//TEST CODE
-assertArraysequal(middle([1]), []); //pass, no middle of 1 item array
-assertArraysequal(middle([1, 2]), []); //pass, no middle of 2 item array
-assertArraysequal(middle([1, 2, 3]), [2]); //pass, middle is 2
-assertArraysequal(middle([1, 2, 3, 4]), [2, 3]) //pass, middle is 2, 3
+module.exports = middle;
