f50286e5ee6107e8b392085d789bcca125e76b13
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-18 20:38:35 -0400

    Set up empty function with some test code

---
+++ Phase_1/loblow/middle.js
@@ -0,0 +1,15 @@
+// Write a function that returns the middle of an array
+ 
+const assertArraysequal = require('./assertArraysEqual');
+
+const middle = (array) => {
+  const result = [];
+
+  return result;
+}
+
+//TEST CODE
+assertArraysequal(middle([1]), []); //pass, no middle of 1 item array
+assertArraysequal(middle([1, 2]), []); //pass, no middle of 2 item array
+assertArraysequal(middle([1, 2, 3], [2])); //pass, middle is 2
+assertArraysequal(middle([1, 2, 3, 4], [2, 3])) //pass, middle is 2, 3
