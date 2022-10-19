99316c324b2f39472e2b0f9435680373951be4c3
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-18 20:59:46 -0400

    Implement function

--- Phase_1/loblow/middle.js
+++ Phase_1/loblow/middle.js
@@ -5,6 +5,19 @@ const assertArraysequal = require('./assertArraysEqual');
 const middle = (array) => {
   const result = [];
 
+  if (array.length > 2) {
+    if (array.length % 2 === 0) {
+      const lowerMiddle = array[(array.length/2 - 1)];
+      const upperMiddle = array[(array.length/2)];
+      result.push(lowerMiddle);
+      result.push(upperMiddle);
+    }
+    if (array.length % 2 !== 0) {
+      const middle = array[Math.floor(array.length/2)];
+      result.push(middle);
+    }
+  }
+
   return result;
 }
 
