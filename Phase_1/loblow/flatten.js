76d84ade7e4359ec61737cfc45447b502c18a855
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-19 19:10:27 -0400

    Task: Write a function which takes an array of items and nested arrays, and returns one flattened array

---
+++ Phase_1/loblow/flatten.js
@@ -0,0 +1,11 @@
+//Write a function which takes an array with items and nested arrays, and returns one flattened array
+
+const flatten = (array) => {
+  resultArray = [];
+
+  console.log(resultArray);
+  return resultArray;
+}
+
+//TEST CODE
+flatten([1, 2, [3, 4], 5, [6]]);
