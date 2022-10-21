43e992b8e8252e2de58a64026f1ab0d3da76441d
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-10-20 23:50:53 -0400

    Task: Write a script that takes a command line arg and prints it out obfuscated. a =4, e = 3, o = 0, l = 1

---
+++ Phase_1/password.js
@@ -0,0 +1,11 @@
+//Write a script that takes a command line string and prints out the obfuscated version with the following rules
+// "a" replaced with "4"
+// "e" replaced with "3"
+// "o" replaced with "0"
+// "l" replaced with "1"
+
+const obfuscate = (string) => {
+  let result = "";
+  return result;
+}
+
